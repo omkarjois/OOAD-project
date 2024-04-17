f2a0fd70cace3d3eab4a1e12433c535b0ffc6a5d
omkarjois <omkarjois2003@gmail.com>
2024-04-17 22:30:06 +0530

    a

--- Bank-Management-System/src/bank/management/system/Signup.java
+++ Bank-Management-System/src/bank/management/system/Signup.java
@@ -18,13 +18,6 @@ public class Signup extends JFrame implements ActionListener {
     Signup(){
         super ("SIGN UP");
 
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/bank.png"));
-        Image i2 = i1.getImage().getScaledInstance(100,100,Image.SCALE_DEFAULT);
-        ImageIcon i3 = new ImageIcon(i2);
-        JLabel image = new JLabel(i3);
-        image.setBounds(25,10,100,100);
-        add(image);
-
         JLabel label1 = new JLabel("APPLICATION FORM NO."+ first);
         label1.setBounds(160,20,600,40);
         label1.setFont(new Font("Raleway",Font.BOLD,38));
